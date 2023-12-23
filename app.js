b1dd1d144bd9b6388f17b2ee37a22ca71a6224b8
fmfahath <fmfahath.dev@gmail.com>
2023-12-24 02:36:04 +0530

    password validation function added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
  let showPassBtn = document.querySelector('.show-password');
  let passwordInp = document.querySelector('.password-input');
+ let passwordChecklist = document.querySelectorAll('.list-item');
 
 
  //show/hide password in input field
@@ -8,4 +9,27 @@
     showPassBtn.classList.toggle('fa-eye-slash');
 
     passwordInp.type = passwordInp.type === 'password' ? 'text' : 'password';
+ });
+
+ //ReqularExpression validation
+ let validationRegex = [
+    {regex : /.{8,}/}, 
+    {regex : /[0-9]/},
+    {regex : /[a-z]/},
+    {regex : /[A-Z]/},
+    {regex : /[^A-Za-z0-9]/}
+ ];
+
+ passwordInp.addEventListener('keyup', () => {
+    validationRegex.forEach((item, i) => {
+
+        let isValid = item.regex.test(passwordInp.value);
+
+        if(isValid){
+            passwordChecklist[i].classList.add('checked');
+        }
+        else{
+            passwordChecklist[i].classList.remove('checked');            
+        }
+    })
  });
